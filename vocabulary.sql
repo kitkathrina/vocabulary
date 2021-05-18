050e5a4d4105bbfdb6ef07ab9cdf1651d686c7f1
kitkathrina <kat.city.of.fallen.angels@gmail.com>
2021-05-18 16:56:58 -0700

    Add create table statements

---
+++ vocabulary.sql
@@ -0,0 +1,58 @@
+DROP TABLE Learns;
+DROP TABLE Contains;
+DROP TABLE User;
+DROP TABLE Word;
+DROP TABLE Material;
+
+CREATE TABLE STATEMENTS
+CREATE TABLE Material (
+title VARCHAR2(50),
+author VARCHAR2(50),
+type VARCHAR2(20),
+PRIMARY KEY (title)
+);
+
+GRANT ALL PRIVILEGES ON Material TO Public;
+
+CREATE TABLE Word (
+word VARCHAR2(20),
+definition VARCHAR2(250),
+language VARCHAR2(20),
+partOfSpeech VARCHAR2(20),
+pronunciation VARCHAR2(50),
+PRIMARY KEY (word)
+);
+
+GRANT ALL PRIVILEGES ON Word TO Public;
+
+CREATE TABLE User (
+username VARCHAR2(20),
+password VARCHAR2(50),
+PRIMARY KEY (username)
+);
+
+GRANT ALL PRIVILEGES ON User TO Public;
+
+CREATE TABLE Contains (
+sentence VARCHAR2(250),
+title VARCHAR2(50),
+word VARCHAR2(20),
+PRIMARY KEY (title, word),
+FOREIGN KEY (title) REFERENCES Material,
+FOREIGN KEY (word) REFERENCES Word
+);
+
+GRANT ALL PRIVILEGES ON Contains TO Public;
+
+CREATE TABLE Learns (
+date DATE,
+sentence VARCHAR2(20),
+username VARCHAR2(20),
+word VARCHAR2(20),
+word VARCHAR2(20),
+PRIMARY KEY (word, user),
+FOREIGN KEY (username) REFERENCES User,
+FOREIGN KEY (word) REFERENCES Word
+);
+
+GRANT ALL PRIVILEGES ON Learns TO Public;
